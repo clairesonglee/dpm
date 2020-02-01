a7eef854b05dff875ea84b7ae330810661ded264
clairesonglee <clairesonglee@gmail.com>
2020-02-01 16:38:27 -0600

    added testcode
    create_ncmpi
    open_ncmpi
    Makefile

---
+++ testcode/create_ncmpi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <pnetcdf.h>
+
+int main (void) { 
+	MPI_Init(NULL, NULL);
+
+	int err_create, err_close, ncid, cmode = NC_CLOBBER | NC_64BIT_DATA;
+	MPI_Info info;
+
+err_create = ncmpi_create(MPI_COMM_WORLD, "foo.nc", cmode, MPI_INFO_NULL, &ncid);
+	if (err_create != NC_NOERR) printf("Error: %s\n",ncmpi_strerror(err_create));
+	
+
+	/* create dimensions, variables, attributes, write/read variables */
+
+	err_close = ncmpi_close(ncid);       /* close netCDF file */
+	if (err_close != NC_NOERR) printf("Error: %s\n",ncmpi_strerror(err_close));
+
+	MPI_Finalize();
+	return 0; 
+}
---
+++ testcode/open_ncmpi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <pnetcdf.h>
+    
+int main (void) {
+
+	MPI_Init(NULL, NULL);
+ 
+	int err, ncid;
+	MPI_Info info;
+    
+	MPI_Info_create (&info);
+	MPI_Info_set (info, "romio_no_indep_rw", "true");
+	MPI_Info_set (info, "nc_header_read_chunk_size", "1024");
+    
+	err = ncmpi_open(MPI_COMM_WORLD, "foo.nc", NC_NOWRITE, info,  &ncid);
+	if (err != NC_NOERR) printf("Error: %s\n",ncmpi_strerror(err));
+	MPI_Info_free(&info);
+
+	MPI_Finalize();
+	return 0; 
+}
