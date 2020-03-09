e1b424332aacd953ae48e94053d3f82d55450b12
clairelee <clairesonglee@gmail.com>
2020-03-09 00:43:09 -0500

    sync ncmpi added

---
+++ testcode/sync_ncmpi.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <pnetcdf.h>
+    
+int main (void) {
+
+	MPI_Init(NULL, NULL);
+ 
+	int err, ncid;
+	MPI_Info info;
+    
+	MPI_Info_create (&info);
+	MPI_Info_set (info, "romio_no_indep_rw", "true");
+	MPI_Info_set (info, "nc_header_read_chunk_size", "1024");
+    
+	err = ncmpi_open(MPI_COMM_WORLD, "foo.nc", NC_NOWRITE, info,  &ncid);
+	if (err != NC_NOERR) printf("Error: %s\n",ncmpi_strerror(err));
+	MPI_Info_free(&info);
+
+	err = ncmpi_redef(ncid); /* enter define mode */
+	if (err != NC_NOERR) printf("Error: %s\n",ncmpi_strerror(err));
+
+	err = ncmpi_enddef(ncid); /* exit define mode */
+	if (err != NC_NOERR) printf("Error: %s\n",ncmpi_strerror(err));
+
+	err = ncmpi_sync(ncid); /* synchronize to disk */
+	if (err != NC_NOERR) printf("Error: %s\n",ncmpi_strerror(err));
+
+	MPI_Finalize();
+	return 0; 
+}
