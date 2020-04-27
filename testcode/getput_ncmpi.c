0801cf10ce3c46f6e7c545aacecc5bcd74a9c49a
clairelee <clairesonglee@gmail.com>
2020-04-27 03:29:47 -0500

    operation not collective solved by adding begin/end indep mode

--- testcode/getput_ncmpi.c
+++ testcode/getput_ncmpi.c
@@ -29,21 +29,25 @@ int main (void) {
 	double var = 1.0; /* initialize temporary variable */
 
 	/* independently write single data value to file */
+	err = ncmpi_begin_indep_data(ncid);
 	err = ncmpi_put_var1_double(ncid, varid, NULL, &var);
-	if (err != NC_NOERR) printf("ncmpi_put_var1_double: %s", ncmpi_strerror(err));
+	if (err != NC_NOERR) printf("ncmpi_put_var1_double: %s\n", ncmpi_strerror(err));
+	err = ncmpi_end_indep_data(ncid);
 
 	/* collectively write single data value to file */
 	err = ncmpi_put_var1_double_all(ncid, varid, NULL, &var);
-	if (err != NC_NOERR) printf("ncmpi_put_var1_double_all: %s", ncmpi_strerror(err));
+	if (err != NC_NOERR) printf("ncmpi_put_var1_double_all: %s\n", ncmpi_strerror(err));
 
 	/* independently read single data value from file */
+	err = ncmpi_begin_indep_data(ncid);
 	err = ncmpi_get_var1_double(ncid, varid, NULL, &var);
-	if (err != NC_NOERR) printf("ncmpi_get_var1_double: %s", ncmpi_strerror(err));
+	if (err != NC_NOERR) printf("ncmpi_get_var1_double: %s\n", ncmpi_strerror(err));
 	if (var != 1.0) printf("ncmpi_get_var1_double: unexpected value"); 
+	err = ncmpi_end_indep_data(ncid);
 
 	/* collectively read single data value from file */
 	err = ncmpi_get_var1_double_all(ncid, varid, NULL, &var);
-	if (err != NC_NOERR) printf("ncmpi_get_var1_double_all: %s", ncmpi_strerror(err));
+	if (err != NC_NOERR) printf("ncmpi_get_var1_double_all: %s\n", ncmpi_strerror(err));
 	if (var != 1.0) printf("ncmpi_get_var1_double_all: unexpected value"); 
 
 
